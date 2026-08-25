b1e46eabf2c7fdfce96777418bdd428b448bae8c
Sudeshna V <165807017+ijust-try@users.noreply.github.com>
2026-08-25 11:44:23 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,66 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        // Check rows
+        for(int i = 0; i < board.length; i++)
+        {
+            HashSet<Character> seen = new HashSet<>();
+
+            for(int j = 0; j < board.length; j++)
+            {
+                if(board[i][j] != '.')
+                {
+                    if(!seen.add(board[i][j]))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // Check columns
+        for(int j = 0; j < board.length; j++)
+        {
+            HashSet<Character> seen = new HashSet<>();
+
+            for(int i = 0; i < board.length; i++)
+            {
+                if(board[i][j] != '.')
+                {
+                    if(!seen.add(board[i][j]))
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        // Check 3 × 3 boxes
+        for(int r = 0; r < 3; r++)
+        {
+            for(int c = 0; c < 3; c++)
+            {
+                HashSet<Character> seen1 = new HashSet<>();
+
+                for(int i = 0; i < 3; i++)
+                {
+                    for(int j = 0; j < 3; j++)
+                    {
+                        int row = r * 3 + i;
+                        int column = c * 3 + j;
+
+                        if(board[row][column] != '.')
+                        {
+                            if(!seen1.add(board[row][column]))
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
