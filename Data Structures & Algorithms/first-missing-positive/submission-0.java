719870e189d8c8013263c51c5fd0e0ed774fe176
Sudeshna V <165807017+ijust-try@users.noreply.github.com>
2026-09-24 14:14:49 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+        int n= nums.length;
+        boolean[]seen= new boolean[n+1];
+
+        for(int num:nums)
+        {
+            if(num>=1 && num<=n)
+            {
+                seen[num]=true;
+            }
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            if(!seen[i])
+                return i;
+        }
+
+        return n+1;
+        
+    }
+}
